c8f18740563eab923066968421b8abdb00f03c95
somayehhosseinbeig <somayeh.hosseinbeig@gmail.com>
2026-05-30 12:13:53 +0330

    Initial commit

---
+++ website/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+def index_view(request):
+    return render(request,'website/index.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def contact_view(request):
+    return render(request,'contact.html')
+
+
