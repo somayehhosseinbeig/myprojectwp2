88e31d000269e7a96e47bcd9f9fcf29bc33ab143
somayehhosseinbeig <somayeh.hosseinbeig@gmail.com>
2026-06-06 16:02:07 +0330

    django Forms and Starting DB

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
-tasks = ["teaching", "cooking","workout","watch Film"]
+# Global Variable as tasks
+# tasks = ["teaching", "cooking","workout","watch Film"]
+# tasks = []
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority =forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    message = forms.CharField(widget=forms.Textarea)
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html",{
-        "tasks":tasks
+        "tasks":request.session["tasks"]
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        # task = request.post.get("task")
+        if  form.is_valid():
+            task = form.cleaned_data["task"]
+            # tasks.append(task)
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request,"tasks/add.html",{
+                "form":form
+            })
+    return render(request, "tasks/add.html",{
+        "form":NewTaskForm()
+    })
